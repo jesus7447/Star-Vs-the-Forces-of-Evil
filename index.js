29293a492f663b63310dc124a4559dee118a4414
User <j_camacho8@yahoo.com>
2022-08-24 22:37:32 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//The idea is to get star to her desination
+//be able to navigate through maze
+//alone the way she can pick up items
+//points will tracked
+//items will have different value
+//different items will cause her to lose or gain points
+
+//create star
+//create items
+//create end point
+//create maze
+//creat obstacles
