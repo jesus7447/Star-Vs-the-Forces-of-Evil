0d5c3506d6845f56185d5e215444dec0b80122dd
User <j_camacho8@yahoo.com>
2022-09-07 21:45:38 -0700

    final commit

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ they thought it was really fun and cool. I didn't really know what I wanted to d
 recently finished called Star vs the Forces of Evil. I did not know how I could make a game like this. I searched ideas online and found some pretty interesting 
 maze games. I thought that would be a great game to make. Have a picture of the main character Star navigate through a maze to get to a destination. It would be simple
 enought game that will push me and also let my daughters enjoy.
+In this game Star will be able to find items which will give her points. Some items 
+may dock points. Ultimatley her goal is to arrive at a destination with the most points possible. 
--- index.js
+++ index.js
@@ -1,12 +1,187 @@
-//The idea is to get star to her desination
-//be able to navigate through maze
-//alone the way she can pick up items
-//points will tracked
-//items will have different value
-//different items will cause her to lose or gain points
+let level = [];
 
-//create star
-//create items
-//create end point
-//create maze
-//creat obstacles
+level[0] = {
+    maze: [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ],
+    player: {
+        x: 0,
+        y: 1
+
+    },
+    goal: {
+        x: 14,
+        y: 13
+    },
+    theme: 'default'
+};
+
+class Maze {
+    constructor(id, level) {
+        this.element = document.getElementById(id);
+        this.tileTypes = ['floor', 'wall'];
+        this.tileDim = 30;
+        this.maze = level.maze;
+        this.theme = level.theme;
+        this.player = { ...level.player};
+        this.goal = { ...level.goal};
+    }
+
+    populateMaze() {
+        this.element.className = 'game-container ' + this.theme;
+        let tiles = document.getElementById('tiles');
+        for (var y = 0; y < this.maze.length; ++y) {
+            for (var x = 0; x < this.maze[y].length; ++x) {
+                let tileCode = this.maze[y][x];
+                let tileType = this.tileTypes[tileCode];
+                let tile = this.createEl(x, y, tileType);
+                tiles.appendChild(tile);
+            }
+        }
+    }
+
+    createEl(x, y, type) {
+        let element = document.createElement('div');
+        element.className = type;
+        element.style.width = element.style.height = this.tileDim + 'px';
+        element.style.left = x * this.tileDim + 'px';
+        element.style.top = y * this.tileDim + 'px';
+        return element;
+    }
+
+    placeSprite(type) {
+        let x = this[type].x;
+        let y = this[type].y;
+        let sprite = this.createEl(x, y, type);
+        sprite.id = type;
+        let layer = this.element.querySelector('#sprites');
+        layer.appendChild(sprite);
+        return sprite;
+    }
+
+    sizeUp() {
+        let maze = this.element.querySelector('.game-map');
+        maze.style.height = this.maze.length * this.tileDim + 'px';
+        maze.style.width = this.maze[0].length * this.tileDim + 'px';
+    }
+
+    movePlayer(event) {
+        if (event.keyCode < 37 || event.keyCode > 40) {
+            return;
+        }
+        switch (event.keyCode) {
+            case 37:
+                this.moveLeft();
+                break;
+            case 38:
+                this.moveUp();
+                break;
+            case 39:
+                this.moveRight();
+                break;
+            case 40:
+                this.moveDown();
+                break;
+        }
+    }
+
+    findMarco() {
+        let body = document.querySelector('body');
+        if (this.player.y == this.goal.y &&
+            this.player.x == this.goal.x) {
+            body.className = 'success';
+        }
+        else {
+            body.className = '';
+        }
+    }
+
+    keyboardListener() {
+        document.addEventListener('keydown', event => {
+            this.movePlayer(event);
+            this.findMarco();
+        });
+    }
+
+    moveLeft() {
+        if (this.player.x == 0) {
+            return;
+        }
+        let nextTile = this.maze[this.player.y][this.player.x - 1];
+        if (nextTile == 1) {
+            return;
+        }
+        this.player.x -= 1;
+        this.updateHoriz();
+    }
+
+    moveUp() {
+        if (this.player.y == 0) {
+            return;
+        }
+        let nextTile = this.maze[this.player.y - 1][this.player.x];
+        if (nextTile == 1) {
+            return;
+        }
+        this.player.y -= 1;
+        this.updateVert();
+    }
+
+    moveRight() {
+        if (this.player.x == this.maze[this.player.y].length - 1) {
+            return;
+        }
+        let nextTile = this.maze[this.player.y][this.player.x + 1];
+        if (nextTile == 1) {
+            return;
+        }
+        this.player.x += 1;
+        this.updateHoriz();
+    }
+
+    moveDown() {
+        if (this.player.y == this.maze.length - 1) {
+            return;
+        }
+        let nextTile = this.maze[this.player.y + 1][this.player.x];
+        if (nextTile == 1) {
+            return;
+        }
+        this.player.y += 1;
+        this.updateVert();
+    }
+
+    updateHoriz() {
+        this.player.element.style.left = this.player.x * this.tileDim + 'px';
+    }
+
+    updateVert() {
+        this.player.element.style.top = this.player.y * this.tileDim + 'px';
+    }
+}
+
+const createMaze = () => {
+    let myMaze = new Maze('game-container-1', level[0]);
+    myMaze.populateMaze();
+    myMaze.sizeUp();
+    myMaze.placeSprite('goal');
+    let playerSprite = myMaze.placeSprite('player');
+    myMaze.player.element = playerSprite;
+    myMaze.keyboardListener();
+}
+
+createMaze();
